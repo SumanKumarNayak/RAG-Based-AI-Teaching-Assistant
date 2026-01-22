f12be4e668adbd06a0b9fb91298162cbe676c974
Suman <suman.200320@gmail.com>
2026-01-22 14:13:44 +0530

    Create prompt from retrieved chunks and generate LLM response

--- process_incoming.py
+++ process_incoming.py
@@ -13,6 +13,18 @@ def create_embedding(text_list):
     embedding = r.json()["embeddings"] 
     return embedding
 
+def inference(prompt):
+    r = requests.post("http://localhost:11434/api/generate", json={
+        # "model": "deepseek-r1:latest",
+        "model": "llama3.2",
+        "prompt": prompt,
+        "stream": False
+    })
+
+    response = r.json()
+    # print(response)
+    return response
+
 df = joblib.load('embeddings.joblib')
 
 incoming_query = input("Ask a Question: ")
@@ -21,12 +33,30 @@ question_embedding = create_embedding([incoming_query])[0]
 # Find similarity of question_embedding with other embeddings
 similarities = cosine_similarity(np.vstack(df['embedding']), [question_embedding]).flatten()
 # print(similarities)
-top_results = 30
+top_results = 5
 max_indx = similarities.argsort()[::-1][0:top_results]
 # print(max_indx)
 new_df = df.loc[max_indx]
 # print(new_df[["title", "number", "text"]])
 
+prompt = f'''I am teaching web development from a course. Here are video chunks containing video title, video number. start time in second,
+end time in seconds, the text at that time:
 
-for index, item in new_df.iterrows():
-    print(index, item["title"], item["number"], item["text"], item["start"], item["end"])
+{new_df[["title", "number", "start", "end", "text"]].to_json(orient="records")}
+---------------------------------------------
+"{incoming_query}"
+User asked this question related to the video chunks, you have to answer in a human way (dont mention the above foramt, 
+its just for you) here and how much content is taught where (in which video and at what timestamp) and guide the user to 
+go to that particular video. If user asks unrelated questiion, tell him that you can only answer questions related to the course
+'''
+with open("prompt.txt", "w") as f:
+    f.write(prompt)
+
+response = inference(prompt)["response"]
+print(response)
+
+with open("response.txt", "w") as f:
+    f.write(response)
+
+# for index, item in new_df.iterrows():
+#     print(index, item["title"], item["number"], item["text"], item["start"], item["end"])
